d927ce247d80dccf64598afd5600f1470b985bab
vitoraqdev <vitor.aquino70@gmail.com>
2023-02-08 20:41:38 -0300

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+use email_newsletter::run;
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind to port 8000.");
+    run(listener)?.await
+}
---
+++ tests/health_check.rs
@@ -0,0 +1,30 @@
+use email_newsletter::run;
+use std::net::TcpListener;
+
+#[tokio::test]
+async fn health_check_works() {
+    // Arrange
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+
+    // Act
+    let response = client
+        .get(format!("{address}/health_check"))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+// Launch the application in the background
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port.");
+    let port = listener.local_addr().unwrap().port();
+    let server = run(listener).expect("Failed to bind address.");
+    let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{port}")
+}
